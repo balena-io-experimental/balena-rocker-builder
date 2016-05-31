8b4ab99aae9d09259c6aef4063eb72982c8be31c
Trong Nghia Nguyen <nghiant2710@gmail.com>
2016-05-31 14:09:50 +0700

    Add package name to output path

--- build.sh
+++ build.sh
@@ -8,10 +8,10 @@ export GOOS=linux
 
 # Build Rocker
 cd ${GOPATH}/src/github.com/grammarly/rocker
-go build -ldflags "-X main.Version=$ROCKER_VERSION" -o ${GOPATH}/bin/
+go build -ldflags "-X main.Version=$ROCKER_VERSION" -o ${GOPATH}/bin/rocker
 
 # Build Rocker-compose
 cd ${GOPATH}/src/github.com/grammarly/rocker-compose
-go build -ldflags "-X main.Version=$ROCKER_COMPOSE_VERSION" -o ${GOPATH}/bin/
+go build -ldflags "-X main.Version=$ROCKER_COMPOSE_VERSION" -o ${GOPATH}/bin/rocker-compose
 
 cp -f ${GOPATH}/bin/* /output/
